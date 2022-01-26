bf2d1ad5637099c781c182b054920470ad825faa
Kane Bruce <kbruce230@gmail.com>
2022-01-26 02:22:43 -0700

    Implement RLCA (0x07)

--- GBCboy/include/gbcpu/cpu.h
+++ GBCboy/include/gbcpu/cpu.h
@@ -11,37 +11,45 @@ struct gbc_reg_t
 	union
 	{
 		struct {
-			uint8_t F;	// flag register
-			uint8_t A;
+			/// <summary>
+			///	<c>0bZNHC0000</c><para/>
+			/// 7 = Z: Zero flag (0x80)<para/>
+			///	6 = N: Subtract flag (0x40)<para/>
+			///	5 = H: Half Carry flag (0x20)<para/>
+			///	4 = C: Carry flag (0x10)<para/>
+			///	Rest are unused. (F: 0-7)
+			/// </summary>
+			uint8_t F;
+			uint8_t A;	// register A (8-15)
 		};
 		uint16_t AF;
 	};
 	union
 	{
 		struct {
-			uint8_t C;
-			uint8_t B;
+			uint8_t C;	// register C (0-7)
+			uint8_t B;	// register B (8-15)
 		};
 		uint16_t BC;
 	};
 	union
 	{
 		struct {
-			uint8_t E;
-			uint8_t D;
+			uint8_t E;	// register E (0-7)
+			uint8_t D;	// register D (8-15)
 		};
 		uint16_t DE;
 	};
 	union
 	{
 		struct {
-			uint8_t L;
-			uint8_t H;
+			uint8_t L;	// register L (0-7)
+			uint8_t H;	// register H (8-15)
 		};
 		uint16_t HL;
 	};
-	uint16_t SP;
-	uint16_t PC;
+	uint16_t SP;	// stack pointer
+	uint16_t PC;	// program counter
 }
 extern gbc_regs;
 
@@ -50,9 +58,10 @@ extern gbc_regs;
 /// </summary>
 struct cpu_instruction_t
 {
-	char* asm;
-	uint8_t op_len;
-	void* execute;
+	char* asm;		// visible format string for the instruction
+	uint8_t op_len;	// length of operands in bytes
+	uint8_t cycles;	// number of cycles to execute
+	void* execute;	// function pointer to execution for instruction
 } extern const gbc_optable[256];
 
 extern char* gbc_cpu_errmsg;
--- GBCboy/src/cpu/cpu.c
+++ GBCboy/src/cpu/cpu.c
@@ -2,20 +2,28 @@
 
 void nop(uint8_t* operands) { return; }
 void ld_bc_d16(uint8_t* operands) { gbc_regs.BC = *((uint16_t*)operands); }
+// TODO: rewrite this to index into memory at BC
 void ld_bc_a(uint8_t* operands) { gbc_regs.BC = 0xff00 + gbc_regs.A; }
 void inc_bc(uint8_t* operands) { gbc_regs.BC += 1; }
 void inc_b(uint8_t* operands) { gbc_regs.B += 1; }
 void dec_b(uint8_t* operands) { gbc_regs.B -= 1; }
 void ld_b_d8(uint8_t* operands) { gbc_regs.B = *operands; }
+void rlca(uint8_t* operands)
+{
+	// rotate left carry A - fancy bit ops shift CF to lowest bit of A
+	gbc_regs.F = 0x10 & (gbc_regs.A = (0xFE & (gbc_regs.A << 1)) | (0x01 & (gbc_regs.F >> 4)));
+	gbc_regs.F = 0xE0 & 0x00; // reset flags
+}
 
 const struct cpu_instruction_t gbc_optable[256] = {
-	/*0x00*/{ "NOP", 0, nop },
-	/*0x01*/{ "LD BC, 0x%04X", 2, ld_bc_d16 },
-	/*0x02*/{ "LD (BC), A", 0, ld_bc_a },
-	/*0x03*/{ "INC BC", 0, inc_bc },
-	/*0x04*/{ "INC B", 0, inc_b },
-	/*0x05*/{ "DEC B", 0, dec_b },
-	/*0x06*/{ "LD B, 0x%02X", 1, ld_b_d8 }
+	/*0x00*/{ "NOP", 0, 4, nop },
+	/*0x01*/{ "LD BC, 0x%04X", 2, 12, ld_bc_d16 },
+	/*0x02*/{ "LD (BC), A", 0, 8, ld_bc_a },
+	/*0x03*/{ "INC BC", 0, 8, inc_bc },
+	/*0x04*/{ "INC B", 0, 4, inc_b },
+	/*0x05*/{ "DEC B", 0, 4, dec_b },
+	/*0x06*/{ "LD B, 0x%02X", 1, 8, ld_b_d8 },
+	/*0x07*/{ "RLCA", 0, 4, rlca }
 };
 
 char* gbc_cpu_errmsg = NULL;
